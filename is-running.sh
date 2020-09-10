24c9dccab64e04f7b30b7c925794f4de46d5f9eb
Alex Burkey <awburkey@gmail.com>
2020-09-10 06:42:43 +0100

    Update script names so the process names don't collide with the other bot

--- is-running.sh
+++ is-running.sh
@@ -10,12 +10,12 @@ date
 # TODO: Might also want to check if there are any python3 
 # processes running.
 # We could get multiple instances of bot processes running
-if [[ $(pgrep run.sh) ]]; then
+if [[ $(pgrep run-ql.sh) ]]; then
     echo "Bot is running"
 else
     echo "Bot is NOT running"
     echo "Restarting bot..."
     # Sends output of run.sh to ./logs/monitor
     # info about when and how many times the bot has crashed
-    nohup ./run.sh >> "./logs/monitor/$(date +"%FT%H%M%S").out" &
+    nohup ./run-ql.sh >> "./logs/monitor/$(date +"%FT%H%M%S").out" &
 fi
